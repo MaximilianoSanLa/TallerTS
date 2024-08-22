88df9018b2c833ce5cca19bdb8f88e8e35c9ec38
maximilianosanla <maximiliano.sanchez.lara@gmail.com>
2024-08-21 20:18:31 -0800

    Taller 2

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import HomePageView,AboutPageView,contact
+from .views import HomePageView,AboutPageView,contact,ProductShowView,ProductCreateView,ProductIndexView
 urlpatterns = [
     path("", HomePageView.as_view(), name='home'),
     path('about/', AboutPageView.as_view(), name='about'),
-    path('contact/', contact.as_view(),name='contact')
+    path('contact/', contact.as_view(),name='contact'),
+    path('products/', ProductIndexView.as_view(), name='index'),
+     path('products/create', ProductCreateView.as_view(), name='form'),
+    path('products/<str:id>', ProductShowView.as_view(), name='show'),
 ]
